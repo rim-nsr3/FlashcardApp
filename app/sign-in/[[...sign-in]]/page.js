ed2bcc26f20e92c1d0f0081cf275cc0a883c2cb5
rim-nsr3 <113094831+rim-nsr3@users.noreply.github.com>
2024-08-20 00:24:48 -0500

    added the design to the remaining files

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,30 +1,19 @@
 import { SignIn } from "@clerk/nextjs";
 import { AppBar, Box, Button, Container, Toolbar, Typography } from "@mui/material";
 import Link from "next/link";
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
 export default function SignUpPage() {
-    return <Container maxWidth="100vw">
-        <AppBar position="static">
-            <Toolbar>
-                <Typography
-                    variant="h6"
-                    sx={{
-                        flexGrow: 1,
-                    }}
-                >
-                    Flashcard SaaS
-                </Typography>
-                <Button color="inherit">
-                    <Link href="/sign-in" passHref>
-                        Login
-                    </Link>
-                </Button>
-                <Button color="inherit">
-                    <Link href="/sign-up" passHref>
-                        Sign Up
-                    </Link>
-                </Button>
-            </Toolbar>
+    return <Container maxWidth="100vh" style={{ backgroundColor: '#F5F5DC' }}>
+        <AppBar position="sticky" sx={{ backgroundColor: '#AFE1AF', boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)' }}>
+          <Toolbar><Typography variant="h6"sx={{flexGrow:1, fontFamily: 'Arial, sans-serif', fontWeight: 'bold' }}>
+          <Link href="/" style={{ color: '#2c3e50', textDecoration: 'none' }}>
+            Flashcard SaaS
+          </Link></Typography><SignedOut><Button sx={{ fontFamily: 'Arial, sans-serif', backgroundColor: '#F5F5DC', color: '#2c3e50', borderRadius: '20px', padding: '8px 16px', minWidth: '120px', marginRight: '16px', '&:hover': { backgroundColor: '#50C878' } }} href="/sign-in">
+              LOG IN
+            </Button><Button sx={{ fontFamily: 'Arial, sans-serif', backgroundColor: 'transparent', color: '#2c3e50', borderRadius: '20px', padding: '8px 16px', border: '2px solid #F5F5DC', minWidth: '120px', '&:hover': { backgroundColor: '#50C878' } }} href="/sign-up">
+              SIGN UP
+            </Button></SignedOut><SignedIn><UserButton /></SignedIn></Toolbar>
         </AppBar>
 
         <Box
@@ -32,8 +21,10 @@ export default function SignUpPage() {
             flexDirection="column"
             alignItems="center"
             justifyContent="center"
+            marginTop={1}
+            height="100vh" 
         >
-            <Typography variant="h4">Sign In</Typography>
+            
             <SignIn />
         </Box>
     </Container>
